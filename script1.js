e1dc14acf0eaf18696ed69d9fdd44b149a95707c
1 <147516506+yushihuanglol@users.noreply.github.com>
2024-10-27 23:49:27 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.getElementById('pumpkin').addEventListener('click', function() {
+    const cat = document.getElementById('cat');
+    cat.style.display = 'block'; // Показываем кота
+});
+s
